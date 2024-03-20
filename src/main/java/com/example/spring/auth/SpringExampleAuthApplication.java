95376558ee45bf297572ee6bdfa1c71bc0d557bd
Muller Strydom <external.muller.strydom@kuehne-nagel.com>
2024-03-20 22:00:29 +0100

    initial spring project

---
+++ src/main/java/com/example/spring/auth/SpringExampleAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExampleAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExampleAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spring/auth/SpringExampleAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExampleAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
