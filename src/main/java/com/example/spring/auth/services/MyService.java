db1031e87e7d8250abafdf4ab22954937fe79856
Muller Strydom <external.muller.strydom@kuehne-nagel.com>
2024-03-21 09:26:39 +0100

    [MethodSecurity] Enable Method security, and add PreAuth to service.

--- src/main/java/com/example/spring/auth/services/MyService.java
+++ src/main/java/com/example/spring/auth/services/MyService.java
@@ -1,10 +1,12 @@
 package com.example.spring.auth.services;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyService {
 
+    @PreAuthorize("hasRole('USER')")
     public String getTestContent() {
         return "Hello World from service";
     }
